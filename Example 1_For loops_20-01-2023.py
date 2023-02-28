7b57c68bfe3a8eff711f115a865174a99d9e2e6d
Harsh Dodiya <harshdodiya1101@gmail.com>
2023-02-28 12:45:49 +0530

    my first commit

---
+++ Example 1_For loops_20-01-2023.py
@@ -0,0 +1,12 @@
+for val in "string":
+    if val == "i":
+        #continue
+        break
+    print(val)
+print("The end")
+#-----------------------------------------------------
+for x in range(10):
+    print(x)
+#-----------------------------------------------------
+pow2=[2**x for x in range(10)]
+print(pow2)
